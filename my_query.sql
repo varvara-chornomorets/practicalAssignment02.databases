ba7f39442e74f1d8416e676ef20a9449dcab59ea
varvara-chornomorets <vchornomorets@kse.org.ua>
2024-10-04 17:11:00 +0300

    top products for customers (smaller sample size)

--- my_query.sql
+++ my_query.sql
@@ -6,14 +6,21 @@ with client_product_cnt as (
     select c.id as client_id, c.name as client_name, p.product_id as product_id,
            p.product_name as product_name, p.description as product_desc, count(o.order_id) as cnt
     from opt_clients c
-    inner join opt_orders o on o.client_id = c.id
+    left join opt_orders o on o.client_id = c.id
     inner join opt_products p on o.product_id = p.product_id
     group by c.id, p.product_id
-    order by cnt
+    order by cnt desc
+),
+ranked_products as (
+    select client_id, client_name, product_id, cnt,
+       rank() over (partition by client_id order by cnt desc) as product_rank
+    from client_product_cnt
+    order by client_id,cnt desc
 )
-select concat(product_name, client_name) from client_product_cnt cpc limit 10 ;
+
+select client_id, client_name, product_id, cnt
+from ranked_products
+where product_rank = 1
+;
 
 
-
-(select concat(product_name, ": ", cnt) from cnt_products where cnt = (select min(cnt) as min_cnt from cnt_products) limit 1) as min_cnt,
-(select concat(product_name, ": ", cnt) from cnt_products where cnt = (select max(cnt) as max_cnt from cnt_products) limit 1) as max_cnt
